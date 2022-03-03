7fac3dfdfaf1d4339239a9a4f729083b381cb7dd
Mehmeteminsahan <emin.sahan@etiya.com>
2022-03-03 16:41:09 +0300

    HelloWorld

--- src/main/java/com/example/hospitalcleaner/HospitalCleanerApplication.java
+++ src/main/java/com/example/hospitalcleaner/HospitalCleanerApplication.java
@@ -8,6 +8,7 @@ public class HospitalCleanerApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(HospitalCleanerApplication.class, args);
+        System.out.println("Hello World");
     }
 
 }
