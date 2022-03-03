9200c70ccbfb9c7a8ecbdb531921c4bb38fa1a93
Mehmeteminsahan <emin.sahan@etiya.com>
2022-03-03 16:35:18 +0300

    firstcommit

---
+++ src/main/java/com/example/hospitalcleaner/HospitalCleanerApplication.java
@@ -0,0 +1,13 @@
+package com.example.hospitalcleaner;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HospitalCleanerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HospitalCleanerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/hospitalcleaner/HospitalCleanerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.hospitalcleaner;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HospitalCleanerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
